85ac2c6fbf3854c576da4a0945897dc2ab324ca3
Colin <colintklein@gmail.com>
2020-07-24 11:30:24 -0500

    Adds login and logout actions

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -28,12 +28,12 @@ const routes = [
     name: 'Logout',
     component: () => import('../views/Logout.vue')
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
