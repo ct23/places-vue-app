1c07643846a4c56090b6ab87a69ee665dee6c7bd
Colin <colintklein@gmail.com>
2020-08-05 12:06:35 -0500

    Ports login page to template

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,45 +1,90 @@
 <template>
   <div class="login">
-
-    <form v-on:submit.prevent="submit()">
-      <h1>Log In</h1>
-      <!-- error handling -->
-      <ul>
-        <li class="text-danger" v-for="error in errors"> {{ error }}</li>
-      </ul>
-      <div class="form-group">
-        <label for="email">Email:</label>
-        <input type="email" id="email" class="form-control" v-model="email">
-      </div>
-      <div class="form-group">
-        <label for="password">Password:</label>
-        <input type="password" id="password" class="form-control" v-model="password">
-      </div>
-      <input type="submit" class="btn btn-primary" value="Submit">
-    </form>
-
-    <router-link to="/createaccount">Create an account</router-link>
-
+    <div class="main-wrapper">
+      <!-- ====================================
+      ———	LOGIN PAGE
+      ===================================== -->
+      <section class="py-7 py-md-10 bg-light ">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-5">
+              <div class="card">
+                <div class="bg-primary text-center py-4">
+                  <h2 class="text-white mb-0 h4">Log In</h2>
+                </div>
+                <div class="card-body px-7 pt-7 pb-0">
+                  <form v-on:submit.prevent="submit()">
+                    <!-- error handling -->
+                    <ul>
+                      <li class="text-danger" v-for="error in errors">
+                        {{ error }}
+                      </li>
+                    </ul>
+                    <div class="form-group mb-7">
+                      <label for="exampleInputText">Email*</label>
+                      <input
+                        required
+                        type="email"
+                        class="form-control"
+                        aria-describedby="emailHelp"
+                        v-model="email"
+                      />
+                    </div>
+                    <div class="form-group mb-7">
+                      <label for="exampleInputPassword1">Password*</label>
+                      <input
+                        required
+                        type="password"
+                        class="form-control"
+                        v-model="password"
+                      />
+                    </div>
+                    <div
+                      class="form-group d-flex justify-content-between align-items-center mb-7"
+                    >
+                      <button
+                        type="submit"
+                        class="btn btn-outline-primary text-uppercase"
+                      >
+                        Log in
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div class="card-footer bg-transparent text-center py-3">
+                  <p class="mb-0">
+                    Not a member yet?
+                    <router-link to="/createaccount" class="link"
+                      >Sign up</router-link
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <!-- element wrapper ends -->
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
 
 <script>
 import axios from "axios";
 
 export default {
-  data: function () {
+  data: function() {
     return {
       email: "",
       password: "",
       errors: [],
     };
   },
-  created: function () {},
+  created: function() {},
   methods: {
-    submit: function () {
+    submit: function() {
       var params = {
         email: this.email,
         password: this.password,
